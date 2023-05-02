f5d28a4382096563d648896a41c09b12f4d31ac9
Saleh Sami <salehsami017@gmail.com>
2023-05-02 08:46:53 +0500

    updating fortuitously

--- MERN_Stack/backend/controller/goalController.js
+++ MERN_Stack/backend/controller/goalController.js
@@ -24,7 +24,15 @@ const setGoal = asyncHandler(async (req, res) => {
 })
 
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({"messgae": `Update goal for ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error("Goal not found")
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+    res.status(200).json(updatedGoal)
 })
     
 const deleteGoal = asyncHandler(async (req, res) => {

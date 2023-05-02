57cb0e2e9d2f6917fc7df293b9ba279da219a3c7
Saleh Sami <salehsami017@gmail.com>
2023-05-02 08:35:05 +0500

    creating successfully

--- MERN_Stack/backend/controller/goalController.js
+++ MERN_Stack/backend/controller/goalController.js
@@ -17,7 +17,10 @@ const setGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add a text field in postman')
     }
-    res.status(200).json({"messgae": "set or Create goal"})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 })
 
 const updateGoal = asyncHandler(async (req, res) => {
